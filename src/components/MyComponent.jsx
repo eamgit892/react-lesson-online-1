94a74ae77d049a69ca57dc58dd1d329a357f6e6b
Yevhen Mozoliak <cutken@gmail.com>
2024-10-16 00:08:56 +0300

    vite probe during online readct lesson 1

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+function MyComponent() {
+    return (
+        <>
+            <h2>my component return 2 </h2>
+            <h3>my component return 3 </h3>
+        </>
+    )
+}
+
+export default MyComponent;
